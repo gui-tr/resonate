1beb8d46d14784daa0f3191cfc83c0a7d405e3e7
gui-tr <gtrionfo@proton.me>
2025-03-13 16:57:35 +0000

    create first flyway db migrations

--- src/main/resources/db/migration/V1.1.0__create_artists_table.sql
+++
@@ -1,6 +0,0 @@
-create table artists (
-    id uuid primary key references profiles(id) on delete cascade,
-    display_name text not null,
-    bio text,
-    profile_image_url text
-);
--- src/main/resources/db/migration/V1.2.0__create_subscriptions_table.sql
+++
@@ -1,8 +0,0 @@
-create table subscriptions (
-    id uuid primary key default gen_random_uuid(),
-    user_id uuid references profiles(id) on delete cascade,
-    status text check (status in ('active', 'canceled', 'expired')) not null,
-    plan text check (plan in ('monthly', 'yearly')) not null,
-    start_date timestamp default now(),
-    end_date timestamp
-);
